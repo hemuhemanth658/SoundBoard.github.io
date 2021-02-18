1756885ae275d8c4e5fac91078842f08448512ed
Hemanth Kumar Narayanam <59231711+hemuhemanth658@users.noreply.github.com>
2021-02-18 15:39:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ['Saahore Bahubali','Sir Ostahra','Kannuladha','Prema Vennala','Naidorintikada']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime=0;
+    })
+}
